e2fb2d82a5f710048f5e47091c43a02255da89e3
Gilson Rodrigues <gilson_rodrigues@hotmail.com>
2025-09-10 19:31:57 -0300

    Mudei a lista de nome de usuarios para tornarem-se objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,4 +1,24 @@
-const usuarios = ['Gilson', 'Kissia', 'Rose', 'Ze']
+const usuarios = [
+    {
+        nome: 'Gilson',
+        email: 'gilson@teste.com'
+    },
+    {
+        nome: 'Kissia',
+        email: 'kissia@teste.com'
+
+    },
+    {
+        nome: 'Barbara',
+        email: 'barbara@teste.com'
+
+    },
+    {
+        nome: 'Antonella',
+        email: 'antonella@teste.com'
+
+    }
+]
 
 function retornarUsuarios() {
     return usuarios;
@@ -6,6 +26,10 @@ function retornarUsuarios() {
 
 function adicionarNovoUsuario(nome) {
     usuarios.push(nome);
+}
+
+function capturarPropiedadeDoUsuario(indiceDoUsuario, propiedade) {
+    return usuarios.at[indiceDoUsuario][propiedade];
 }
 
 module.exports = {
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -5,12 +5,16 @@ describe('Testar as funções de gestão de Usarios', () => {
     it('Validar que posso adiconar um novo usuario na lista', function () {
 
         // 1. Adicionar um nove nome na lista de usuarios 
-        adicionarNovoUsuario('Kaue');
+        adicionarNovoUsuario({
+            nome: 'Kaue',
+            email: 'kaue@teste.com'
+        });
 
         // 2. Retornar a lista de usuarios 
         const listaDeUsuarios = retornarUsuarios();
 
         // 3. Comparar se o novo nome esta no fim da lista de usuarios 
-        expect(listaDeUsuarios.at(-1)).to.equal('Kaue');
+        expect(listaDeUsuarios.at(-1).nome).to.equal('Kaue');
+        expect(listaDeUsuarios.at(-1).email).to.equal('kaue@teste.com');
     })
 });
